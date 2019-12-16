44c79c6104c7c2edf686a9933a67b2cceefc240c
Marina <marina_silva98@hotmail.com>
2019-12-15 23:14:25 -0300

    Versão final (com iem)

--- fuzzy/src/fuzzy/Fuzzy.java
+++ fuzzy/src/fuzzy/Fuzzy.java
@@ -28,9 +28,10 @@ public class Fuzzy {
         String fileName = "regras.fcl";
         FIS fis = FIS.load(fileName, true);
 
-        Double natalidade[] = {21.55, 17.92, 22.41, 23.67, 15.42, 15.08, 15.51, 16.03, 15.02, 15.12, 19.4, 13.18, 16.61, 16.76, 19.62, 15.24, 15.88, 16.06, 14.34, 13.1, 15.41, 16.84, 22.6, 12.28, 13.57, 16.55, 14.19, 18.2};
-        Double mortalidade[] = {5.09, 7.22, 5.53, 4.53, 7.01, 6.36, 7.11, 4.14, 5.49, 5.9, 7.3, 6.12, 5.91, 5.42, 5.75, 8.37, 7.61, 8.18, 5.89, 7.26, 6.5, 6.03, 5.52, 6.63, 5.04, 6.86, 5.82, 6.2};
-
+        Double natalidade[] = {21.55, 17.92, 22.41, 23.67, 15.42, 15.51, 16.03, 15.02, 15.12, 19.4, 13.18, 16.61, 16.76, 19.62, 15.24, 15.88, 16.06, 14.34, 13.1, 15.41, 16.84, 22.6, 12.28, 13.57, 16.55, 14.19, 18.2};
+        Double mortalidade[] = {5.09, 7.22, 5.53, 4.53, 7.01, 7.11, 4.14, 5.49, 5.9, 7.3, 6.12, 5.91, 5.42, 5.75, 8.37, 7.61, 8.18, 5.89, 7.26, 6.5, 6.03, 5.52, 6.63, 5.04, 6.86, 5.82, 6.2};
+        Double migracao[] = {0.6, 0.85, 0.2, 0.14, 0.63, 0.53, 0.43, 0.29, 0.4, 0.78, 0.23, 0.09, 0.13, 0.4, 0.54, 0.611, 0.63, 0.27, 0.29, 0.39, -0.03, 0.02, 0.25, 0.08, 0.45, 0.26, 0.25}; 
+        
         DecimalFormat df = new DecimalFormat("0.00");
         
         // Error while loading?
@@ -44,10 +45,11 @@ public class Fuzzy {
         JFuzzyChart.get().chart(functionBlock);
 
         // Set inputs
-        for (int i = 0; i < 28; i++) {
+        for (int i = 0; i < 27; i++) {
             fis.setVariable("natalidade", natalidade[i]);
             fis.setVariable("mortalidade", mortalidade[i]);
-                
+            fis.setVariable("migracao", migracao[i]);
+                    
             // Evaluate
             fis.evaluate();
         
@@ -55,7 +57,7 @@ public class Fuzzy {
         }
 
         // Show output variable's chart
-        //JFuzzyChart.get().chart(functionBlock.getVariable("crescimento"), functionBlock.getVariable("crescimento").getDefuzzifier(), true);
+        JFuzzyChart.get().chart(functionBlock.getVariable("crescimento"), functionBlock.getVariable("crescimento").getDefuzzifier(), true);
         // Print ruleSet
         //System.out.println(fis);
         
